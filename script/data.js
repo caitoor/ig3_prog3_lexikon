da1f554e9dc3c3993d277479d12f1ca2a260e31c
FR <mail@rauch-fabian.de>
2023-03-28 11:02:44 +0200

    stand der vorlesung nach termin am 22.3.23

---
+++ script/data.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+        "title": "Systemschrift",
+        "description": "Eine Systemschrift ist eine standardisierte Schriftart, die von einem Betriebssystem bereitgestellt wird und von allen Anwendungen verwendet werden kann.",
+        "tags": ["Typografie", "Design", "Betriebssystem"]
+    },
+
+    {
+        "title": "Breakpoint",
+        "short_explanation": "Ein Breakpoint ist eine definierte Breite, an der das Layout einer Website bei der Verwendung von Responsive Webdesign angepasst wird.",
+        "explanation": "Breakpoints werden verwendet, um das Layout einer Website für verschiedene Geräte und Bildschirmgrößen zu optimieren. Sie werden in der Regel in CSS-Medienabfragen definiert und ermöglichen es, das Layout der Website bei bestimmten Bildschirmbreiten anzupassen. Dies ist ein wichtiger Bestandteil des Responsive Webdesigns, das sicherstellt, dass eine Website auf allen Geräten und Bildschirmgrößen optimal dargestellt wird.",
+        "tags": ["Responsive Webdesign", "CSS", "Webentwicklung"]
+    },
+    {
+        "title": "SCSS",
+        "short_explanation": "SCSS steht für Sassy CSS und ist eine Erweiterung der herkömmlichen CSS-Syntax.",
+        "explanation": "SCSS ist eine von Sass (Syntactically Awesome Style Sheets) entwickelte Erweiterung von CSS. Es erweitert die herkömmliche CSS-Syntax um Funktionen wie Variablen, Schleifen und bedingte Anweisungen. SCSS-Code wird in der Regel in einer separate Datei geschrieben und dann in CSS kompiliert, um in einer Website verwendet zu werden.",
+        "tags": ["CSS", "Webentwicklung"]
+    },
+    {
+        "title": "Framework",
+        "short_explanation": "Ein Framework ist eine Sammlung von Software-Tools und Bibliotheken, die zur Entwicklung von Anwendungen verwendet werden können.",
+        "explanation": "Ein Framework ist eine Sammlung von wiederverwendbaren Softwarekomponenten, die Entwicklern dabei helfen, Anwendungen schneller und einfacher zu erstellen. Frameworks sind in der Regel auf bestimmte Programmiersprachen oder Anwendungsbereiche spezialisiert und bieten vorgefertigte Lösungen für häufige Probleme in der Softwareentwicklung. Sie können Bibliotheken, APIs, Werkzeuge und Konventionen enthalten, die Entwicklern helfen, schnellere und qualitativ hochwertigere Anwendungen zu entwickeln.",
+        "tags": ["Softwareentwicklung", "Webentwicklung"]
+    },
+    {
+        "title": "Rest-Schnittstelle",
+        "short_explanation": "Eine REST-Schnittstelle ist ein Standard zur Übertragung von Daten zwischen Systemen über das HTTP-Protokoll.",
+        "explanation": "REST (Representational State Transfer) ist ein Architekturstil für die Entwicklung von Webanwendungen, der es ermöglicht, Daten zwischen verschiedenen Systemen über das HTTP-Protokoll zu übertragen. Eine REST-Schnittstelle definiert eine Reihe von Regeln und Konventionen, die sicherstellen, dass die Übertragung von Daten zwischen Systemen einheitlich und zuverlässig erfolgt. REST-Schnittstellen werden in der Regel verwendet, um Daten zwischen einer Client-Anwendung und einer Server-Anwendung auszutauschen.",
+        "tags": ["Webentwicklung", "HTTP", "Architektur"]
+    },
+    {
+        "title": "API",
+        "short_explanation": "Eine API (Application Programming Interface) ist eine Schnittstelle zur Kommunikation zwischen verschiedenen Softwareanwendungen.",
+        "explanation": "Eine API definiert die Regeln und Mechanismen, die die Interaktion zwischen verschiedenen Softwareanwendungen ermöglichen. Sie ermöglicht die Kommunikation von Anwendungen über ein Netzwerk, indem sie eine standardisierte Schnittstelle bereitstellt. Eine API ermöglicht es Anwendungen, Daten und Funktionalitäten von anderen Anwendungen abzurufen oder bereitzustellen, ohne dass eine direkte Kopplung zwischen den Anwendungen erforderlich ist.",
+        "tags": ["Softwareentwicklung", "Programmierung", "Webentwicklung"]
+    },
+    {
+        "title": "git",
+        "short_explanation": "Git ist ein verteiltes Versionskontrollsystem für die Softwareentwicklung.",
+        "explanation": "Git ist ein weit verbreitetes Versionskontrollsystem für die Softwareentwicklung, das von Linus Torvalds entwickelt wurde. Es ermöglicht es Entwicklern, den Code einer Anwendung in einem gemeinsamen Repository zu speichern und Änderungen an diesem Code zu verfolgen. Git ermöglicht es Entwicklern, Änderungen an einem Code gleichzeitig durchzuführen und Konflikte zu vermeiden. Es ist ein sehr leistungsfähiges Werkzeug für die Softwareentwicklung und wird von vielen Entwicklern und Unternehmen eingesetzt.",
+        "tags": ["Versionskontrolle", "Softwareentwicklung"]
+    },
+    {
+        "title": "npm",
+        "short_explanation": "npm ist ein Package Manager für JavaScript-Module und -Pakete.",
+        "explanation": "npm (Node Package Manager) ist ein Package Manager für JavaScript-Module und -Pakete. Es wird häufig verwendet, um Abhängigkeiten und Bibliotheken für Node.js-basierte Anwendungen zu verwalten. npm ermöglicht es Entwicklern, Module und Pakete zu installieren, zu aktualisieren und zu entfernen, die in ihrer Anwendung benötigt werden. Es ist ein wichtiger Bestandteil der modernen JavaScript-Entwicklung und wird von einer großen Anzahl von Entwicklern und Unternehmen genutzt.",
+        "tags": ["JavaScript", "Webentwicklung"]
+    },
+    {
+        "title": "Package Manager",
+        "short_explanation": "Ein Package Manager ist ein Werkzeug zur Verwaltung von Abhängigkeiten in Softwareprojekten.",
+        "explanation": "Ein Package Manager ist ein Werkzeug zur Verwaltung von Abhängigkeiten in Softwareprojekten. Ein Package Manager ermöglicht es Entwicklern, Bibliotheken und Abhängigkeiten einfach und effektiv zu verwalten, die in einer Anwendung benötigt werden. Es automatisiert den Prozess der Installation, Aktualisierung und Entfernung von Bibliotheken und Abhängigkeiten und ermöglicht es Entwicklern, sich auf das Schreiben von Code zu konzentrieren, anstatt sich um die Verwaltung von Abhängigkeiten zu kümmern.",
+        "tags": ["Softwareentwicklung", "Webentwicklung"]
+    },
+    {
+        "title": "HTML5",
+        "short_explanation": "HTML5 ist die neueste Version der Hypertext Markup Language und wird verwendet, um Webseiten zu erstellen.",
+        "explanation": "HTML5 ist die neueste Version der Hypertext Markup Language, die verwendet wird, um Webseiten zu erstellen. Es ist ein wichtiger Bestandteil der Webentwicklung und wird von Entwicklern verwendet, um Inhalte und Struktur von Webseiten zu definieren. HTML5 bietet viele neue Funktionen und APIs, die es Entwicklern ermöglichen, interaktive und ansprechende Webseiten zu erstellen. Es ist ein wichtiger Bestandteil des modernen Web und wird von allen gängigen Webbrowsern unterstützt.",
+        "tags": ["Webentwicklung", "HTML"]
+    }
+]
---
+++ script/script.js
@@ -0,0 +1,17 @@
+{/* <div class="tile">
+<h3>Repository</h3>
+<div class="tile-content">
+    <p>Eine kurze Kurzbeschreibung in einem Satz.</p>
+    <div class="tile-tags">
+        <ul>
+            <li>HTML</li>
+            <li>git</li>
+            <li>CSS</li>
+        </ul>
+    </div>
+</div>
+</div> */}
+
+$(function() {
+    const tileContainer = $("#tiles");
+})
