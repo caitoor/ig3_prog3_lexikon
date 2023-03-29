c4a85e069b484ea01ac56aaf1e559218f1e652fd
FR <mail@rauch-fabian.de>
2023-03-29 14:40:09 +0200

    pausenstand 230329

--- script/script.js
+++ script/script.js
@@ -1,17 +1,24 @@
-{/* <div class="tile">
-<h3>Repository</h3>
-<div class="tile-content">
-    <p>Eine kurze Kurzbeschreibung in einem Satz.</p>
-    <div class="tile-tags">
-        <ul>
-            <li>HTML</li>
-            <li>git</li>
-            <li>CSS</li>
-        </ul>
-    </div>
-</div>
-</div> */}
+$(function () {
+    const tileContainer = $("#tile-wrapper");
+    for (let i = 0; i < 7; i++) {
+        const tile = $(`<div class="tile">
+       <h3>${data[i].title}</h3>
+       <div class="tile-content">
+           <p>${data[i].short_explanation}</p>
+           <div class="tile-tags">
+               <ul>
+                   
+               </ul>
+           </div>
+       </div>
+       </div>`);
 
-$(function() {
-    const tileContainer = $("#tiles");
+        const tagList = tile.find(".tile-tags ul");
+        let currentData = data[i];
+        for (let j = 0; j < currentData.tags.length; j++) {
+            let currentTag = currentData.tags[j];
+            tagList.append($(`<li>${currentTag}</li>`));
+        }
+        tileContainer.append(tile);
+    }
 })
